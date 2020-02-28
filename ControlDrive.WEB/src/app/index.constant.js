d04aa9815c31665b1c505239b515c9baafb7f0ad
Alejandro Perez <alejandro.perez@pymesco.com>
2020-02-28 18:52:22 +1100

    fix: prod parameters

--- ControlDrive.WEB/src/app/index.constant.js
+++ ControlDrive.WEB/src/app/index.constant.js
@@ -1,8 +1,7 @@
 (function () {
     'use strict';
-    //var ApiUrl= "http://192.168.0.29/API/";
-    var ApiUrl= "http://localhost:50579/API/";
-    //var ApiUrl = "http://" + location.hostname + "/" + location.pathname.split("/")[1] + "/API/";
+    //var ApiUrl= "http://127.0.0.0:5000/";
+    var ApiUrl = "http://" + location.hostname + "/" + location.pathname.split("/")[1] + "/API/";
     angular.module('controldriveApp')
         .constant('ngAuthSettings', {
             apiServiceBaseUri: ApiUrl,
